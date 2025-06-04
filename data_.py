c778aacd68e137ca039426d20099f730c6ea9800
Zahra Asadi <zahraasadi257@gmail.com>
2025-06-04 04:42:41 -0600

    Initial commit with selected modules from MalCL

---
+++ data_.py
@@ -0,0 +1,35 @@
+
+import numpy as np
+import torch
+import torch.nn as nn
+import random
+
+
+def get_ember_train_data(data_dir):
+
+    XY_train = np.load(data_dir + '/XY_train.npz')
+    X_train, Y_train = XY_train['X_train'], XY_train['Y_train']
+
+    return X_train, Y_train
+
+
+def get_ember_test_data(data_dir):
+    XY_test = np.load(data_dir + '/XY_test.npz')
+    X_test, Y_test = XY_test['X_test'], XY_test['Y_test']
+    # A  is a PyTorch tensor designed to hold integer data. It's often used for labels in machine learning, especially when performing classification tasks where each label corresponds to a class index.
+    Y_test = torch.LongTensor(Y_test)
+
+    return X_test, Y_test
+
+def shuffle_data(x_, y_, s):
+    random.seed(s)
+    indices = list(range(len(x_)))
+    random.shuffle(indices)
+    x_ = x_[indices]
+    y_ = y_[indices]
+    return x_, y_
+
+def oh(Y, num_classes):
+    Y = torch.FloatTensor(Y)
+    Y_oh = nn.functional.one_hot(Y.to(torch.int64), num_classes=num_classes)
+    return Y_oh
